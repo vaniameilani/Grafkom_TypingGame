fd8913f4ff9d8f09d3f60dd717d47cfa9b312d40
qqdnada <nada.qori@gmail.com>
2021-01-11 13:43:40 +0800

    lighting awan + nambah kata

--- TypingGame/cloudBackground.js
+++ TypingGame/cloudBackground.js
@@ -22,6 +22,27 @@ var HEIGHT, WIDTH,
 
 var clouds = [];
 
+var ambientLight, hemisphereLight, shadowLight;
+
+function createLights() {
+
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+  shadowLight.position.set(150, 350, 350);
+  shadowLight.castShadow = true;
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+}
+
 //INIT THREE JS, SCREEN AND MOUSE EVENTS
 
 function createScene() {
@@ -184,7 +205,7 @@ function normalize( v, vmin, vmax, tmin, tmax ){
 function init(event){
     document.addEventListener('mousemove', handleMouseMove, false);
     createScene();
-    // createLights();
+    createLights();
     createSea();
     createSun();
     createSky();
